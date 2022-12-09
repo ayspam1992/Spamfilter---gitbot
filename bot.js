f684c8d99ef93ea552e45b37937dddfab9f56657
ayspam1992 <113533672+ayspam1992@users.noreply.github.com>
2022-12-09 16:50:57 +0300

    update
    2

--- bot.js
+++ bot.js
@@ -1,17 +1,16 @@
 const Discord = require("discord.js");
 const keepAlive = require("./server")
-const config = require("./config.json");
 let cld = new Discord.Collection();
-let wb = config.webhook.split(["/"]);
-config.webhookID = wb[5];
-config.webhookToken = wb[6];
+let wb = process.env.webhook.split(["/"]);
+webhookID = wb[5];
+webhookToken = wb[6];
 
 const client = new Discord.Client();
 client.on("ready", async () => {
   console.log("Bot Hazır: " + client.user.tag);
 });
 client.on("message", async (msg) => {
-  if (msg.channel.id != config.channel) return;
+  if (msg.channel.id != process.env.channel) return;
   let id = msg.content.match(/🆔 ?[0-9]*/gi)[0];
   if (!id) return;
 
@@ -20,8 +19,8 @@ client.on("message", async (msg) => {
   msg.react("✅");
   let now = new Date().getTime();
   const webhookClient = new Discord.WebhookClient(
-    config.webhookID,
-    config.webhookToken
+    webhookID,
+    webhookToken
   );
   let content = msg.content;
   webhookClient.send(`@everyone\n${content}`);
@@ -32,4 +31,4 @@ client.on("message", async (msg) => {
 });
 
 keepAlive()
-client.login(config.token);
+client.login(process.env.token);
